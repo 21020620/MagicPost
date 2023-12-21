d0602f21ebe2923abb2e750ad0c6ef03967d5c32
thanhdatdo <thanhdatdo2k3@gmail.com>
2023-12-21 16:34:57 +0700

    add addEmployerForm

---
+++ FE/client/src/components/Layout/AddEmployerForm.jsx
@@ -0,0 +1,181 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  Form,
+  Input,
+  Select,
+  DatePicker
+} from 'antd';
+const { Option } = Select;
+
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+const AddEmployerForm = () => {
+  const [form] = Form.useForm();
+  const onFinish = (fieldsValue) => {
+    //Format date value before submit.
+    const values = {
+        'date-picker': fieldsValue['date-picker'].format('YYYY-MM-DD'),
+    };
+    console.log('Received values of form: ', values);
+};
+
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    </Form.Item>
+  );
+
+  return (
+    <Form
+      {...formItemLayout}
+      form={form}
+      name="add"
+      onFinish={onFinish}
+      style={{
+        maxWidth: 600,
+      }}
+      scrollToFirstError
+    >
+        <Form.Item
+        name="firstName"
+        label="First Name"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your first name!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="lastName"
+        label="Last Name"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your last name!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item name="date-picker" label="Date Of Birth" >
+      <DatePicker />
+      </Form.Item>
+
+      <Form.Item
+        name="address"
+        label="Address"
+        rules={[
+          {
+            required: true,
+            message: 'Please select your address!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="phone"
+        label="Phone Number"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your phone number!',
+          },
+        ]}
+      >
+        <Input
+          addonBefore={prefixSelector}
+          style={{
+            width: '100%',
+          }}
+        />
+      </Form.Item>
+
+      <Form.Item
+        name="role"
+        label="Role"
+        rules={[
+          {
+            required: true,
+            message: 'Please select your role!',
+          },
+        ]}
+      >
+        <Select placeholder="select your role">
+          <Option value="Transaction Manager">Transaction Manage</Option>
+          <Option value="Central Manager">Central Manager</Option>
+        </Select>
+      </Form.Item>
+
+
+      <Form.Item {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit">
+          Check
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default AddEmployerForm;
--- FE/client/src/components/Layout/CEOmanagerAccount.jsx
+++ FE/client/src/components/Layout/CEOmanagerAccount.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+/* import { useEffect, useState } from 'react';
 import { Button } from 'antd';
 import AccountTable from '../AccountTable';
 import axiosInstance from '../DefaultAxios';
@@ -37,4 +37,80 @@ const CEOmanagerAccount = () => {
     </div>
   );
 };
+export default CEOmanagerAccount; */
+
+import { useEffect, useState } from 'react';
+import { Button, Modal } from 'antd';
+import AccountTable from '../AccountTable';
+import axiosInstance from '../DefaultAxios';
+import AddEmployerForm from './AddEmployerForm';
+
+const CEOmanagerAccount = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await axiosInstance.get('/api/admin/employees');
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log('useEffect triggered.');
+    fetchData();
+  }, []);
+
+  const handleAddButtonClick = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleAddEmployee = () => {
+    // Implement the logic to add an employee here
+    // After adding, you can refetch data or update the table as needed
+    fetchData();
+    setIsModalVisible(false);
+  };
+
+  if (loading) {
+    return <p>Loading data...</p>;
+  }
+
+  return (
+    <div>
+      <Button type="primary" style={{ marginBottom: 16, float: "left" }} onClick={handleAddButtonClick}>
+        Add
+      </Button>
+      <AccountTable data={data} />
+      <Modal
+        //title="Add Employee"
+        visible={isModalVisible}
+        onCancel={handleModalClose}
+        footer={[
+          <Button key="cancel" onClick={handleModalClose}>
+            Cancel
+          </Button>,
+          <Button key="add" type="primary" onClick={handleAddEmployee}>
+            Add
+          </Button>,
+        ]}
+        width={700}
+      >
+        {/* Display the message "kek" in the modal */}
+        <AddEmployerForm/>
+      </Modal>
+    </div>
+  );
+};
+
 export default CEOmanagerAccount;
+
