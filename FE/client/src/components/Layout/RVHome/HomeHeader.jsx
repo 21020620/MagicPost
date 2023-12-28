e15cd79f9bb2898cd7d22e4f78b20a1b533d6c95
thanhdatdo <thanhdatdo2k3@gmail.com>
2023-12-28 16:04:42 +0700

    navigate homepage to login

--- FE/client/src/components/Layout/RVHome/HomeHeader.jsx
+++ FE/client/src/components/Layout/RVHome/HomeHeader.jsx
@@ -8,7 +8,7 @@ import {
   EnvironmentOutlined,
 } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
-import logo from "../../../img/icon.png"
+import logo from "../../../img/icon.png";
 import { Divider, Button } from "antd";
 import "../../footer.css";
 
@@ -18,82 +18,94 @@ export default function HomeHeader() {
     {
       text: "Trang chủ",
       icon: <HomeOutlined />,
-      onClick: () => navigate("/"), 
+      onClick: () => navigate("/"),
     },
     {
       text: "Về chúng tôi",
       icon: <AuditOutlined />,
-      onClick: () => navigate("/about"), 
+      onClick: () => navigate("/about"),
     },
     {
       text: "Tra cứu vận đơn",
       icon: <FileSearchOutlined />,
-      onClick: () => navigate("/search"), 
+      onClick: () => navigate("/search"),
     },
     {
       text: "Giá cước phục vụ",
       icon: <DollarCircleOutlined />,
-      onClick: () => window.open("https://drive.google.com/file/d/1SAVyehyUTDTp9IamGfi_OOKgKs-MKTUY/view?usp=sharing"),
+      onClick: () =>
+        window.open(
+          "https://drive.google.com/file/d/1SAVyehyUTDTp9IamGfi_OOKgKs-MKTUY/view?usp=sharing"
+        ),
     },
     {
       text: "Địa điểm tập kết",
       icon: <EnvironmentOutlined />,
       onClick: () => navigate("/TapKet"),
-    }
+    },
   ];
+
+  const handleLoginButtonClick = () => {
+    navigate("/login");
+  };
 
   return (
     <>
       <div style={{ position: "absolute", left: 10 }}>
-        <img id="headerLogo" src={logo} alt="logo" style={{ width: 120, height: 120 }} />
+        <img
+          id="headerLogo"
+          src={logo}
+          alt="logo"
+          style={{ width: 120, height: 120 }}
+        />
       </div>
       <Divider />
-        <div className="navbar-container">
-          <div className="navbar-links-container">
-            {menuOptions.map((option, index) => (
-              <a
-                key={index}
-                href=""
-                className="menu-option"
-                onClick={option.onClick}
-              >
-                {option.icon}
-                {option.text}
-              </a>
-            ))}
-          </div>
-          <Button
-            size="large"
-            className="button-hover"
-            style={{
-              position: "absolute",
-              top: 10,
-              right: 10,
-              height: 50,
-              width: 150,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <LoginOutlined style={{ marginRight: 8 }} />
-              <span
-                style={{
-                  fontWeight: "bold",
-                  fontSize: 15,
-                  maxWidth: 120,
-                  wordWrap: "break-word",
-                  whiteSpace: "normal",
-                  lineHeight: 1.2,
-                }}
-              >
-                Đăng nhập
-              </span>
-            </div>
-          </Button>
+      <div className="navbar-container">
+        <div className="navbar-links-container">
+          {menuOptions.map((option, index) => (
+            <a
+              key={index}
+              href=""
+              className="menu-option"
+              onClick={option.onClick}
+            >
+              {option.icon}
+              {option.text}
+            </a>
+          ))}
         </div>
-      {/* </Divider> */}
+        <Button
+          size="large"
+          className="button-hover"
+          style={{
+            position: "absolute",
+            top: 10,
+            right: 10,
+            height: 50,
+            width: 150,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onClick={handleLoginButtonClick}
+        >
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <LoginOutlined style={{ marginRight: 8 }} />
+            <span
+              style={{
+                fontWeight: "bold",
+                fontSize: 15,
+                maxWidth: 120,
+                wordWrap: "break-word",
+                whiteSpace: "normal",
+                lineHeight: 1.2,
+              }}
+            >
+              Đăng nhập
+            </span>
+          </div>
+        </Button>
+      </div>
     </>
   );
 }
